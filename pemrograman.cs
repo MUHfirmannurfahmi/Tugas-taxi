f1ba3a12270fcfbdfe282fa0a996c23b87b143c3
User <fhmiheluput33@gmail.com>
2023-03-21 23:04:21 +0700

    first commit

---
+++ pemrograman.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    class pemrograman
+    {
+        static void Main(string[] args)
+        {
+
+
+            Taxi taxi = new Taxi();
+            Taxi taxi1 = new Taxi();
+
+            taxi.DriverName = "fahmi";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 2845633;
+
+            taxi1.DriverName = "Budi";
+            taxi1.OnDuty = true;
+            taxi1.NumPassenger = 4745777;
+
+            taxi.info();
+            taxi.ada();
+            taxi.Tidak();
+
+            taxi1.info();
+            taxi1.ada();
+            taxi1.Tidak();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ taxi.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxi
+{
+    public class Taxi
+    {
+        public string DriverName { get; set; }
+        public Boolean OnDuty { get; set; }
+        public int NumPassenger { get; set; }
+
+        public void info()
+        {
+            Console.WriteLine("\n Information Taxi \n");
+            Console.WriteLine(" Nama Driver : {0}", DriverName);
+
+            if (OnDuty == true)
+            {
+                Console.WriteLine(" Kerja : Iya", OnDuty);
+            }
+            else
+            {
+                Console.WriteLine(" Kerja : Tidak", OnDuty);
+            }
+
+            Console.WriteLine(" Nomor Pelanggan : {0}", NumPassenger);
+        }
+
+        public void ada()
+        {
+            Console.WriteLine(" {0} Maaf Sedang Ada Penumpang", DriverName);
+        }
+        public void Tidak()
+        {
+            Console.WriteLine(" {0} Sedang Tidak Ada Penumpang", DriverName);
+        }
+    }
+}
